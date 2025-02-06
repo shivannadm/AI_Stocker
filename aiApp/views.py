934e0ff43e43e4834d5f450c6e6ca1eacb96951a
shivannadm <shivannadm6@gmail.com>
2025-02-06 08:26:48 +0530

    App Added

---
+++ aiApp/views.py
@@ -0,0 +1,154 @@
+from django.shortcuts import render
+
+# Create your views here.
+def basefile(request):
+    return render(request, 'base.html')
+
+def lt_view(request):
+    return render(request, 'lt.html')
+
+def maruti_view(request):
+    return render(request, 'maruti.html')
+
+def mm_view(request):
+    return render(request, 'mm.html')
+
+def nestle_view(request):
+    return render(request, 'nestle.html')
+
+def ntpc_view(request):
+    return render(request, 'ntpc.html')
+
+def ongc_view(request):
+    return render(request, 'ongc.html')
+
+def powergrid_view(request):
+    return render(request, 'powergrid.html')
+
+def reliance_view(request):
+    return render(request, 'reliance.html')
+
+def sbiin_view(request):
+    return render(request, 'sbiin.html')
+
+def sbilife_view(request):
+    return render(request, 'sbilife.html')
+
+def shreecem_view(request):
+    return render(request, 'shreecem.html')
+
+def sunpharm_view(request):
+    return render(request, 'sunpharm.html')
+
+def tataconsumer_view(request):
+    return render(request, 'tataconsumer.html')
+
+def tatamotors(request):
+    return render(request, 'tatamotors.html')
+
+def tatasteel_view(request):
+    return render(request, 'tatasteel.html')
+
+def tcs_view(request):
+    return render(request, 'tcs.html')
+
+def techm_view(request):
+    return render(request, 'techm.html')
+
+def titan_view(request):
+    return render(request, 'titan.html')
+
+def ultracem_view(request):
+    return render(request, 'ultracem.html')
+
+def upl_view(request):
+    return render(request, 'upl.html')
+
+def wipro_view(request):
+    return render(request, 'wipro.html')
+
+# new added for utkarsh's data
+
+def apse_view(request):
+    return render(request, 'apse.html')
+
+def aspn_view(request):
+    return render(request, 'aspn.html')
+
+def axisbank_view(request):
+    return render(request, 'axisbank.html')
+
+def bajajauto_view(request):
+    return render(request, 'bajajauto.html')
+
+def bajajfin_view(request):
+    return render(request, 'bajajfin.html')
+
+def bjfn_view(request):
+    return render(request, 'bjfn.html')
+
+def bpcl_view(request):
+    return render(request, 'bpcl.html')
+
+def britania_view(request):
+    return render(request, 'britania.html')
+
+def brtair_view(request):
+    return render(request, 'brtair.html')
+
+def cipla_view(request):
+    return render(request, 'cipla.html')
+
+def coal_view(request):
+    return render(request, 'coal.html')
+
+def divilab_view(request):
+    return render(request, 'divilab.html')
+
+def dredlab_view(request):
+    return render(request, 'dredlab.html')
+
+def eichermotor_view(request):
+    return render(request, 'eichermotor.html')
+
+def grsm_view(request):
+    return render(request, 'grsm.html') 
+
+def hcltech_view(request):
+    return render(request, 'hcltech.html')
+
+def hdfcbank_view(request):
+    return render(request, 'hdfcbank.html')
+
+def hdfclife_view(request):
+    return render(request, 'hdfclife.html')
+
+def heromotor_view(request):
+    return render(request, 'heromotor.html')
+
+def hindalco_view(request):
+    return render(request, 'hindalco.html')
+
+def hul_view(request):
+    return render(request, 'hul.html')
+
+def icibank_view(request):
+    return render(request, 'icibank.html')
+
+def indusbank_view(request):
+    return render(request, 'indusbank.html')
+
+def infosys_view(request):
+    return render(request, 'infosys.html')
+
+def ioc_view(request):
+    return render(request, 'ioc.html')
+
+def itc_view(request):
+    return render(request, 'itc.html')
+
+def jswsteel_view(request):
+    return render(request, 'jswsteel.html')
+
+def kotakbank_view(request):
+    return render(request, 'kotakbank.html')
